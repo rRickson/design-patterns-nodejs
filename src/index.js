4efae85eecb531c5108359ce33c80e92a80b8260
Rickson Vasconcelos <rickson.vasc@gmail.com>
2022-07-11 08:43:20 -0300

    Add Prototype Design to open the server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 
 const SingletonDB = require('./designs/Singleton')
 const portChecker = require('./designs/ChainOfResp')
-const express = require("express");
-const app = express();
-const port = 8080
-app.on('error', err => {
-    console.log(
-        "err")
-})
-app.get("/init", (req, res) => {
+const Server = require('./designs/Prototype')
 
+const server = new Server(8080);
+server.listen();
+
+server.express.get("/init", (req, res) => {
     /**
      * Singleton is most used for instances that can be called allways with the same state.
      * always. DB Connection is one of the most types that use singleton
@@ -25,12 +22,10 @@ app.get("/init", (req, res) => {
         }
 
     } catch (e) {
+        console.log(e)
         res.status(401).send(`ERROR ON PORT CHECK`);
 
     }
 
 });
 
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-});
