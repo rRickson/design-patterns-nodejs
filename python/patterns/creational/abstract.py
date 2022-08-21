7bb6f0242a79e649dabf6c921ceab636f086c6ee
Rickson Vasconcelos <rickson.vasc@gmail.com>
2022-08-21 18:27:46 -0300

    Fixing Abstract

--- python/patterns/creational/abstract.py
+++ python/patterns/creational/abstract.py
@@ -13,90 +13,30 @@ class AbstractFactory(ABC):
         pass
 
 
-class ConcreteFactory1(AbstractFactory):
-    def create_product_a(self) -> AbstractProductA:
-        return ConcreteProductA1()
-
-    def create_product_b(self) -> AbstractProductB:
-        return ConcreteProductB1()
-
-
-class ConcreteFactory2(AbstractFactory):
-
-    def create_product_a(self) -> AbstractProductA:
-        return ConcreteProductA2()
-
-    def create_product_b(self) -> AbstractProductB:
-        return ConcreteProductB2()
-
-
-class AbstractProductA(ABC):
-
-    @abstractmethod
-    def useful_function_a(self) -> str:
+class AbstractProductA:
+    def useful_abstraction_a(self) -> str:
+        pass
+class AbstractProductB:
+    def useful_abstraction_b(self) -> str:
         pass
 
 
-class ConcreteProductA1(AbstractProductA):
-    def useful_function_a(self) -> str:
-        return "The result of the product A1."
+class ProductA(AbstractProductA):
+    def useful_abstraction_a(self) -> str:
+        return 'Product A'
 
+class ProductB(AbstractProductA):
+    def useful_abstraction_b(self) -> str:
+        return 'Product B'
 
-class ConcreteProductA2(AbstractProductA):
-    def useful_function_a(self) -> str:
-        return "The result of the product A2."
-
-
-class AbstractProductB(ABC):
-
-    @abstractmethod
-    def useful_function_b(self) -> None:
-
-        pass
-
-    @abstractmethod
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
-
-        pass
-
-class ConcreteProductB1(AbstractProductB):
-    def useful_function_b(self) -> str:
-        return "The result of the product B1."
-
-
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
-        result = collaborator.useful_function_a()
-        return f"The result of the B1 collaborating with the ({result})"
-
-
-class ConcreteProductB2(AbstractProductB):
-    def useful_function_b(self) -> str:
-        return "The result of the product B2."
-
-    def another_useful_function_b(self, collaborator: AbstractProductA):
-        result = collaborator.useful_function_a()
-        return f"The result of the B2 collaborating with the ({result})"
-
-
-def client_code(factory: AbstractFactory) -> None:
-    product_a = factory.create_product_a()
-    product_b = factory.create_product_b()
-
-    print(f"{product_b.useful_function_b()}")
-    print(f"{product_b.another_useful_function_b(product_a)}", end="")
+    def second_abstraction_b(self) -> str:
+        return 'second abstraction b'
 
 
 if __name__ == "__main__":
-    """
-    The client code can work with any concrete factory class.
-    """
-    print("Client: Testing client code with the first factory type:")
-    client_code(ConcreteFactory1())
-
-    print("\n")
-
-    print("Client: Testing the same client code with the second factory type:")
-    client_code(ConcreteFactory2())
-
+    print(ProductB().useful_abstraction_b())
+    print('\n')
+    print(ProductB().useful_abstraction_a())
+    print('\n')
 # Since this will be a delivery message framework Abstract doesn't see much sense since it will be straight forward
 # Use the Abstract Factory when your code needs to work with various families of related products, but you don’t want it to depend on the concrete classes of those products—they might be unknown beforehand or you simply want to allow for future extensibility.
