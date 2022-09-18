ec2bd1621349ddebb84354f0ba2c949552fa54b2
Rickson Vasconcelos <rickson.vasc@gmail.com>
2022-09-18 12:40:00 -0300

    end creational

--- python/patterns/creational/abstract.py
+++ python/patterns/creational/abstract.py
@@ -1,42 +1,167 @@
-
-from __future__ import annotations
 from abc import ABC, abstractmethod
 
+class AbstractMobileUIFactory(ABC):
+	"""
+	Class representing app UI and the supported features
+	"""
 
-class AbstractFactory(ABC):
-    @abstractmethod
-    def create_product_a(self) -> AbstractProductA:
-        pass
+	@abstractmethod
+	def create_story(self):
+		pass
 
-    @abstractmethod
-    def create_product_b(self) -> AbstractProductB:
-        pass
+	@abstractmethod
+	def upload_photo(self):
+		pass
+
+class AndroidFactory(AbstractMobileUIFactory):
+	"""
+	Class for Android related features
+	"""
+
+	def create_story(self):
+		print("Inside AndroidFactory class")
+		AndroidCreateStory().story()
+
+	def upload_photo(self):
+		print("Inside AndroidFactory class")
+		AndroidUploadPhoto().photo()
+
+class IosFactory(AbstractMobileUIFactory):
+	"""
+	Class for iOS related features
+	"""
+
+	def create_story(self):
+		print("Inside IosFactory class")
+		IosCreateStory().story()
+
+	def upload_photo(self):
+		print("Inside IosFactory class")
+		IosUploadPhoto().photo()
+
+class SymbianFactory(AbstractMobileUIFactory):
+	"""
+	Class for Symbian related features
+	"""
+
+	def create_story(self):
+		print("Inside SymbianFactory class")
+		SymbianCreateStory().story()
+
+	def upload_photo(self):
+		print("Inside SymbianFactory class")
+		SymbianUploadPhoto().photo()
+
+class AbstractCreateStory(ABC):
+	"""
+	Class to provide create story feature
+	"""
+
+	@abstractmethod
+	def story(self):
+		pass
 
 
-class AbstractProductA:
-    def useful_abstraction_a(self) -> str:
-        pass
-class AbstractProductB:
-    def useful_abstraction_b(self) -> str:
-        pass
+class AndroidCreateStory(AbstractCreateStory):
+	"""
+	Class to create story on Android platform
+	"""
+
+	def story(self):
+		print("[Android] Creating story on android platform.")
+
+class IosCreateStory(AbstractCreateStory):
+	"""
+	Class to create story on iOS platform
+	"""
+
+	def story(self):
+		print("[IOS] Creating story on IOS platform.")
+
+class SymbianCreateStory(AbstractCreateStory):
+	"""
+	Class to create story on Symbian platform
+	"""
+
+	def story(self):
+		print("[Symbian] Ok boomer! Creating story on Symbian platform, while we can!!")
 
 
-class ProductA(AbstractProductA):
-    def useful_abstraction_a(self) -> str:
-        return 'Product A'
+class AbstractUploadPhoto(ABC):
+	"""
+	Class to provide upload photo feature
+	"""
 
-class ProductB(AbstractProductA):
-    def useful_abstraction_b(self) -> str:
-        return 'Product B'
+	@abstractmethod
+	def photo(self):
+		pass
 
-    def second_abstraction_b(self) -> str:
-        return 'second abstraction b'
+class AndroidUploadPhoto(AbstractUploadPhoto):
+	"""
+	Class to upload photo on Android platform
+	"""
 
+	def photo(self):
+		print("[Android] Uploading photo on android platform.")
 
-if __name__ == "__main__":
-    print(ProductB().useful_abstraction_b())
-    print('\n')
-    print(ProductB().useful_abstraction_a())
-    print('\n')
-# Since this will be a delivery message framework Abstract doesn't see much sense since it will be straight forward
-# Use the Abstract Factory when your code needs to work with various families of related products, but you don’t want it to depend on the concrete classes of those products—they might be unknown beforehand or you simply want to allow for future extensibility.
+class IosUploadPhoto(AbstractUploadPhoto):
+	"""
+	Class to upload photo on iOS platform
+	"""
+
+	def photo(self):
+		print("[IOS] Uploading photo on IOS platform.")
+
+class SymbianUploadPhoto(AbstractUploadPhoto):
+	"""
+	Class to upload photo on Symbian platform
+	"""
+
+	def photo(self):
+		print("[Symbian] So you want us to upload petroglyph? Since we are backward compatible, why not! Uploading photo on Symbian platform")
+
+class Application():
+
+	def get_factory(self, platform_type):
+		factory = None
+		if platform_type == "Android":
+			factory = AndroidFactory()
+		elif platform_type == "IOS":
+			factory = IosFactory()
+		elif platform_type == "Symbian":
+			factory = SymbianFactory()
+		else:
+			print("ERROR: unknown platform type.")
+		return factory
+
+	def create_story(self, factory=None):
+		if not factory:
+			print("factory object not passed")
+		factory.create_story()
+
+	def upload_photo(self, factory=None):
+		if not factory:
+			print("factory object not passed")
+		factory.upload_photo()
+
+if __name__ == '__main__':
+	app_object = Application()
+
+	# in real world scenarios, instead of pass hardcoded platform type we can read platform type from config file.
+	print("Running for Android platform...")
+	factory_object = app_object.get_factory("Android")
+	factory_object.create_story()
+	factory_object.upload_photo()
+	print("\n")
+
+	print("Running for IOS platform...")
+	factory_object = app_object.get_factory("IOS")
+	factory_object.create_story()
+	factory_object.upload_photo()
+	print("\n")
+
+	print("Running for Symbian platform...")
+	factory_object = app_object.get_factory("Symbian")
+	factory_object.create_story()
+	factory_object.upload_photo()
+	print("\n")
--- python/patterns/creational/builder.py
+++ python/patterns/creational/builder.py
@@ -1,6 +1,6 @@
 class Computer:
-    def __init__(self, serial_number):
-        self.serial = serial_number
+    def __init__(self):
+        self.serial = None
         self.memory = None
         self.hdd = None
         self.gpu = None
@@ -8,37 +8,36 @@ class Computer:
     def __str__(self):
         info = (f'Memory: {self.memory}GB',
                 f'Hard Disk: {self.hdd}GB',
-                f'Graphics Card: {self.gpu}')
+                f'Graphics Card: {self.gpu}',
+                f'Serial: {self.serial}')
         return '\n'.join(info)
 
 class ComputerBuilder:
+    
     def __init__(self):
-        self.computer = Computer('AG23385193')
-    def configure_memory(self, amount):
+        self.computer = Computer()
+
+    def set_serial(self, serial):
+        self.computer.serial = serial
+        return self 
+    def set_memory(self, amount):
         self.computer.memory = amount
-    def configure_hdd(self, amount):
+        return self
+    def set_hdd(self, amount):
         self.computer.hdd = amount
-    def configure_gpu(self, gpu_model):
+        return self 
+    def set_gpu(self, gpu_model):
         self.computer.gpu = gpu_model
+        return self
 
-class HardwareEngineer:
-    def __init__(self):
-        self.builder = None
-    def construct_computer(self, memory, hdd, gpu):
-        self.builder = ComputerBuilder()
-        self.builder.configure_memory(memory)
-        self.builder.configure_hdd(hdd)
-        self.builder.configure_gpu(gpu)
-
-    @property
-    def computer(self):
-        return self.builder.computer
-
-def main():
-    engineer = HardwareEngineer()
-    engineer.construct_computer(hdd=500,memory=8,gpu='GeForce GTX 650 Ti')
-    computer = engineer.computer
-    print(computer)
+    def build(self):
+        return self.computer
 
 if __name__ == '__main__':
-    main()
+    computer = ComputerBuilder()\
+        .set_gpu("GeForce GTX 650 Ti")\
+            .set_hdd('500')\
+                .set_memory("8")\
+                    .set_serial("AG23385193")\
+                        .build()
+    print(computer)
